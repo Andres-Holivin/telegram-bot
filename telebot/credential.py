2d35e8c43dc44b8924a0a0d6f3d2b88d286c1ad7
andres holivin <30800318+holivin21@users.noreply.github.com>
2022-02-03 13:24:03 +0700

    change url

--- telebot/credential.py
+++ telebot/credential.py
@@ -1,3 +1,3 @@
 bot_token = "2061095209:AAE3DaLKskZ2menxpLcccjKyx4Bp5mUCThY"
 bot_user_name = "@HolivinTradingBot"
-URL = "https://holivin-telegram-bot.herokuapp.com/"
+URL = "https://bot-telegram111.herokuapp.com/"
